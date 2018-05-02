43877b1ff08948a85749a95b69086215f202f044
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-02 21:45:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  
+  pigeon_list = {}
+
+  data.each do |color_key, color_birds|
+    color_birds.each do |specific_color_key , name_array|
+      name_array.each do |name|
+        pigeon_list[name] = {}
+      end #name_array each  
+    end #end color_birds each
+  end #end data each
+  pigeon_list
+  binding.pry
+end #end big method
