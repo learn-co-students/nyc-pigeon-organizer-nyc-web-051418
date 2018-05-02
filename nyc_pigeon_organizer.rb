547ba3486330821a0de48cc5edd0801c4b2d38bb
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-02 22:39:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,33 +3,33 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
-  second_hash = {}
-  second_hash_array = []
 
-  data.each do |color_key, color_birds|
-    color_birds.each do |specific_color_key , name_array|
-      name_array.each do |name|
+  data.each_pair do |color_key, color_birds|
+    color_birds.each do |specific_color_key , bottom_level|
+      bottom_level.each do |name|
         
         
         
         #binding.pry
         
         data.each do |color_key, color_birds|
-          color_birds.each do |specific_color_key , name_array|
+          bottom_level.each do |name|
             
             #binding.pry
-            if name_array.include?(name)
+            if bottom_level.include?(name)
               pigeon_list[name] = {}
             end #end if name_array include
           
-            #binding.pry
             
-            if pigeon_list[name][:color].nil? || pigeon_list[name][:color].empty?
-              
+            
+            if pigeon_list[name][:color] == nil
+              #binding.pry
               pigeon_list[name][:color] = []
-              pigeon_list[name][:color] << color_birds
-            end  
-          
+              
+              
+            end 
+            #binding.pry
+          pigeon_list[name][:color] << bottom_level
           
           
             
