0a303da386d8597d60cd46ca9f6c924d69abc44b
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-03 01:31:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,30 +4,38 @@ def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
   
-  data.each do |color_gender_lives_symbols, group_details|
-    group_details.each do |color_gender_lives_specific, bottom_level|
+  data.each do |c_g_l_symbols, group_details|
+    group_details.each do |c_g_l_specific, bottom_level|
       bottom_level.each do |bottom_level_specific|  
         
-        binding.pry
+        #binding.pry
+        if !!pigeon_list[bottom_level_specific]  
+           pigeon_list[bottom_level_specific][c_g_l_symbols] << (c_g_l_specific.to_s)
+           
+        # elsif !!pigeon_list[bottom_level_specific][c_g_l_symbols]
+        #     pigeon_list[bottom_level_specific] = {c_g_l_symbols => [c_g_l_specific.to_s]}
+           
+        else  
+          pigeon_list[bottom_level_specific] = {c_g_l_symbols => [c_g_l_specific.to_s]}
+        end  
         
-        if !!pigeon_list[bottom_level_specific][color_gender_lives_specific.to_s]
-          pigeon_list[bottom_level_specific][color_gender_lives_specific.to_s] << bottom_level_specific
-        
-        elsif !!pigeon_list[bottom_level_specific]
-          pigeon_list[bottom_level_specific][color_gender_lives_symbols] = [bottom_level_specific]
-        else 
-          pigeon_list[bottom_level_specific] = {color_gender_lives_symbols => [color_gender_lives_specific.to_s]}
-        end
-    binding.pry
+       # binding.pry
     
       end #end bottom_level each 
     end #end group details each
   end #end data each
   
   pigeon_list
+  binding.pry
 end #end big method
 
-
+# if !!pigeon_list[bottom_level_specific][color_gender_lives_specific.to_s]
+#           #pigeon_list[bottom_level_specific][color_gender_lives_specific.to_s] << bottom_level_specific
+#         elsif !!pigeon_list[bottom_level_specific]
+#         # pigeon_list[bottom_level_specific][color_gender_lives_symbols] = [bottom_level_specific]
+#         else 
+#           pigeon_list[bottom_level_specific] = {color_gender_lives_symbols => [color_gender_lives_specific.to_s]}
+#         end
 
 
 
