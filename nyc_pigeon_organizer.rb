3a40334581e9de2cab6af6817b079af8540c6aaf
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-07 21:27:41 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,12 +5,12 @@ def nyc_pigeon_organizer(data)
   data.each do |final_bird_key, bird_data|
     bird_data.each do |final_bird_value, bird_names|
       bird_names.each do |name|
+        # initialize to an empty hash, or do nothing
         organized_pigeons[name] ||= {}
-        if organized_pigeons[name][final_bird_key] == nil
-          organized_pigeons[name][final_bird_key] = [final_bird_value.to_s]
-        else
-          organized_pigeons[name][final_bird_key] += [final_bird_value.to_s]
-        end
+        # initialize to an empty array, or do nothing
+        organized_pigeons[name][final_bird_key] ||= [final_bird_value.to_s]
+        # if the array exists, append the array with final_bird_value
+        organized_pigeons[name][final_bird_key] += [final_bird_value.to_s]
       end
     end
   end
