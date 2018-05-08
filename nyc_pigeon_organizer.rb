ceb7446b60867f79d84a91a2f18e597148b1f11b
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-07 21:21:02 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  organized_pigeons = {}
+  data.each do |final_bird_key, bird_data|
+    bird_data.each do |final_bird_value, bird_names|
+      bird_names.each do |name|
+        organized_pigeons[name] ||= {}
+        if organized_pigeons[name][final_bird_key] == nil
+          organized_pigeons[name][final_bird_key] = [final_bird_value.to_s]
+        else
+          organized_pigeons[name][final_bird_key] += [final_bird_value.to_s]
+        end
+      end
+    end
+  end
+  organized_pigeons
 end
