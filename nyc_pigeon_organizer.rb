94b8f91423b400855bac3bc41c4498149bbf780c
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 22:12:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+    pigeon_names = {}
+
+    data.each do |category, info|
+      info.each do |sub_cat, deet|
+        deet.each do |name|
+            pigeon_names[name] ||= {}
+            pigeon_names[name][category] ||= []
+            pigeon_names[name][category] << sub_cat.to_s
+        end
+      end
+    end
+    pigeon_names
 end
